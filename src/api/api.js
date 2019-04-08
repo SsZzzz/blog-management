7caa30cae788e0f0dad295ee21025220c2fea8ca
Mendia <suze0526@gmail.com>
2019-04-08 20:50:51 +0800

    路由增加根路径

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const login = params => { return axios.post('/management/login', params) };
-const getLabel = () => { return axios.get('/management/getLabel') };
-const saveArticle = params => { return axios.post('/management/saveArticle', params) };
-const getSummary = () => { return axios.get('/management/getSummary') };
-const getOneWeekLoginData = () => { return axios.get('/management/getOneWeekLoginData') };
+const login = params => { return axios.post('/blog-serve/management/login', params) };
+const getLabel = () => { return axios.get('/blog-serve/management/getLabel') };
+const saveArticle = params => { return axios.post('/blog-serve/management/saveArticle', params) };
+const getSummary = () => { return axios.get('/blog-serve/management/getSummary') };
+const getOneWeekLoginData = () => { return axios.get('/blog-serve/management/getOneWeekLoginData') };
 
 export default {
   login,
